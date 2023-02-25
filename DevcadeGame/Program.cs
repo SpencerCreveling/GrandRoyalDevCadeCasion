ca64e0020d83cc29fa49577793ee73c9225ca441
SpencerCreveling <spencer.creveling@gmail.com>
2023-02-25 00:45:55 -0500

    black jack changes

--- DevcadeGame/Program.cs
+++ DevcadeGame/Program.cs
@@ -1,5 +1,7 @@
-﻿// This file does not need to be edited.
+﻿using DevcadeGame;
+using System;
+using System.Diagnostics;
+// This file does not need to be edited.
 //using var game = new DevcadeGame.Game1();
 //game.Run();
-
-var BlackJack = new BlackJack();
+var BlackJackGame = new BlackJack(100);
